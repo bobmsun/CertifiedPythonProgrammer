bcba759e64c83ad03d013fff89ea732bb9527940
Shihao Sun <shihao.m.sun@gmail.com>
2022-11-13 23:20:12 -0500

    20221113_associate_ch4_multiple_inheritance

---
+++ associate/ch6_classes_and_object_oriented_programming/single_and_multiple_inheritance/car2.py
@@ -0,0 +1,21 @@
+
+# If a class makes sense to be multiple classes, then we can use multiple inheritence
+
+from vehicle2 import Vehicle
+
+class Car(Vehicle):  
+
+    default_tire = 'tire'
+
+    def __init__(self, engine, tires=None, distance_traveled=0, unit='miles'):
+        # print("car constructor")
+        super().__init__(distance_traveled, unit)
+        if not tires:
+            tires = [self.default_tire, self.default_tire, self.default_tire, self.default_tire]
+        self.tires = tires
+        self.engine = engine
+    
+    def drive(self, distance):
+        self.distance_traveled += distance
+
+
