573d4c65b119b2f60f417787a8f8453f435141af
Shihao Sun <shihao.m.sun@gmail.com>
2022-12-09 15:25:20 -0500

    20221209_entry_add_list_pop_method

--- entry_level/ch9_lists/list_functions_and_methods.py
+++ entry_level/ch9_lists/list_functions_and_methods.py
@@ -16,6 +16,22 @@ print(my_list)             # ['a', 1, 2, 3, 4]
 
 
 
+# 自己加：上面加了往 list 里面加 .append 和 插入，那么如果想从 list 里面删除元素怎么办呢？
+# 用 pop function
+# list.pop(index)
+
+# pop 函数的 parameter
+# The pop() method takes a single argument (index).
+# The argument passed to the method is optional. If not passed, the default index -1 is passed as an argument (index of the last item).
+# If the index passed to the method is not in range, it throws IndexError: pop index out of range exception.
+
+# pop 函数的 return value:
+# The pop() method returns the item present at the given index. This item is also removed from the list.
+
+# https://www.programiz.com/python-programming/methods/list/pop
+
+
+
 
 
 
